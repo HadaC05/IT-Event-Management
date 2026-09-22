5ccbc46fbfa3e828108cd2c285b2ca1af957dfca
Brian Ragasi <ragasibrian2@gmail.com>
2026-09-23 03:54:02 +0800

    Restrict directory regression check to CLI

--- api/test-team-directory-mysql.php
+++ api/test-team-directory-mysql.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+if (PHP_SAPI !== 'cli') {
+    http_response_code(404);
+    exit;
+}
+
 require_once __DIR__.'/student-portal.php';
 require_once __DIR__.'/faculty.php';
 
